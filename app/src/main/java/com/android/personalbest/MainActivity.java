b7961199ff51ec8285e54a5021f5c27ad6aacebb
Nidhi Bangari <nbangari@ucsd.edu>
2019-02-12 17:38:30 -0800

    cleaned up sign in code

--- app/src/main/java/com/android/personalbest/MainActivity.java
+++ app/src/main/java/com/android/personalbest/MainActivity.java
@@ -1,19 +1,14 @@
 package com.android.personalbest;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
-
-import com.android.personalbest.fitness.GoogleFitAdapter;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
@@ -30,14 +25,9 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-import java.util.concurrent.TimeUnit;
-
 public class MainActivity extends AppCompatActivity {
 
-
-    private final int GOOGLE_FIT_PERMISSIONS_REQUEST_CODE = System.identityHashCode(this) & 0xFFFF;
     private SignInButton signInButton;
-
 
     private int RC_SIGN_IN = 1;
     GoogleSignInClient mGoogleSignInClient;
@@ -54,48 +44,28 @@ public class MainActivity extends AppCompatActivity {
     public SharedPreferences sharedPreferences;
     public SharedPreferences.Editor editor;
 
-    private GoogleFitAdapter googleFitAdapter;
-
-
-
-
-
     //Resource In use:https://firebase.google.com/docs/auth/android/google-signin
-    //Log in with google account with firebase
-    //Issue with Cannot resolve symbol default_web_client_id
-    //Fix with hard code default_web_client_id
-    //Docs:
-    //https://stackoverflow.com/questions/37810552/cannot-resolve-symbol-default-web-client-id-in-firebases-android-codelab
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         signInButton = findViewById(R.id.sign_in_button);
-
         mAuth = FirebaseAuth.getInstance();
-
         sharedPreferences = getSharedPreferences(getString(R.string.user_prefs),MODE_PRIVATE);
-
 
         //update log in status here
         login = sharedPreferences.getBoolean(LogInStatus,login);
-
         //update haveInputtedHeight here
         haveInputtedHeight = sharedPreferences.getBoolean(getString(R.string.first_time), haveInputtedHeight);
 
-
         //If first time signing in, ask user for height
         if (login && !haveInputtedHeight) {
-
             startActivity(new Intent(MainActivity.this, InputHeightActivity.class));
-
         }
         //If not first time signing in, go to main page
         else if (login) {
-
             startActivity(new Intent(MainActivity.this, MainPageActivity.class));
-
         }
 
         // Configure sign-in to request the user's ID, email address, and basic
@@ -114,9 +84,6 @@ public class MainActivity extends AppCompatActivity {
                 signIn();
             }
         });
-
-
-
     }
 
     private void signIn() {
@@ -127,7 +94,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
         // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
@@ -136,20 +102,14 @@ public class MainActivity extends AppCompatActivity {
                 GoogleSignInAccount account = task.getResult(ApiException.class);
                 firebaseAuthWithGoogle(account);
             } catch (ApiException e) {
-                // Google Sign In failed, update UI appropriately
+                // Google Sign In failed, log error
                 Log.w(TAG, "Google sign in failed", e);
-                // ...
             }
         }
-
-
-
-
     }
 
     private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
         Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
-
         AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
         mAuth.signInWithCredential(credential)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
@@ -166,38 +126,17 @@ public class MainActivity extends AppCompatActivity {
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "signInWithCredential:failure", task.getException());
-                            Toast.makeText(MainActivity.this,"You are not able to log in to google",Toast.LENGTH_LONG).show();
-                            //updateUI(null);
+                            Toast.makeText(MainActivity.this,"You are not able to log in to Google",Toast.LENGTH_LONG).show();
                         }
-
-                        // ...
                     }
                 });
     }
 
-
-
-
-
-
-
     private void updateUI(FirebaseUser user) {
-
         signInButton.setVisibility(View.GONE);
         GoogleSignInAccount acct = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
 
         startActivity(new Intent(MainActivity.this, InputHeightActivity.class));
-
-
-        if (acct != null) {
-            String personName = acct.getDisplayName();
-            String personGivenName = acct.getGivenName();
-            String personFamilyName = acct.getFamilyName();
-            String personEmail = acct.getEmail();
-            String personId = acct.getId();
-            Uri personPhoto = acct.getPhotoUrl();
-            Toast.makeText(this, "Name of the user: "+ personName +"user id is :" + personId,Toast.LENGTH_LONG).show();
-        }
 
     }
 
