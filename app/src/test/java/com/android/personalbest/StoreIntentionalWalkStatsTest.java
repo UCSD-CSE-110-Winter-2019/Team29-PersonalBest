32344dc852ba1b03414554ff11708ac127a2aded
Lavanya Satyan <lsatyan@ucsd.edu>
2019-02-13 20:54:02 -0800

    added reset test

--- app/src/test/java/com/android/personalbest/StoreIntentionalWalkStatsTest.java
+++ app/src/test/java/com/android/personalbest/StoreIntentionalWalkStatsTest.java
@@ -87,4 +87,28 @@ public class StoreIntentionalWalkStatsTest {
         assertEquals(0, sharedPrefManager.getIntentionalMilesPerHour(dayOfWeek), 0.00);
         assertEquals(0, sharedPrefManager.getIntentionalTimeElapsed(dayOfWeek));
     }
+
+    @Test
+    public void testResetSharedPrefForDay() {
+        int dayOfWeek = Calendar.MONDAY;
+        int intentionalStepsTaken = 5000;
+        float intentionalDistanceInMiles = 5.1f;
+        float intentionalMilesPerHour = 3.3f;
+        int intentionalTimeElapsed = 20;
+
+        sharedPrefManager.storeIntentionalWalkStats(dayOfWeek, intentionalStepsTaken, intentionalDistanceInMiles,
+                intentionalMilesPerHour, intentionalTimeElapsed);
+
+        assertEquals(intentionalStepsTaken, sharedPrefManager.getIntentionalStepsTaken(dayOfWeek));
+        assertEquals(intentionalDistanceInMiles, sharedPrefManager.getIntentionalDistanceInMiles(dayOfWeek), 0.00);
+        assertEquals(intentionalMilesPerHour, sharedPrefManager.getIntentionalMilesPerHour(dayOfWeek), 0.00);
+        assertEquals(intentionalTimeElapsed, sharedPrefManager.getIntentionalTimeElapsed(dayOfWeek));
+
+        sharedPrefManager.resetSharedPrefForDay(dayOfWeek);
+        assertEquals(0, sharedPrefManager.getIntentionalStepsTaken(dayOfWeek));
+        assertEquals(0, sharedPrefManager.getIntentionalDistanceInMiles(dayOfWeek), 0.00);
+        assertEquals(0, sharedPrefManager.getIntentionalMilesPerHour(dayOfWeek), 0.00);
+        assertEquals(0, sharedPrefManager.getIntentionalTimeElapsed(dayOfWeek));
+
+    }
 }
