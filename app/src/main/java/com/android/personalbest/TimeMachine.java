a89a1cce498f263abedda1e3c245679ba13a472f
Lavanya Satyan <lsatyan@ucsd.edu>
2019-02-17 13:39:17 -0800

    added time machine

---
+++ app/src/main/java/com/android/personalbest/TimeMachine.java
@@ -0,0 +1,17 @@
+package com.android.personalbest;
+
+import java.util.Calendar;
+
+//Used for changing day for testing and demo purposes
+public class TimeMachine {
+
+    private static int today = Calendar.DAY_OF_WEEK;
+
+    public static int getDay() {
+        return today;
+    }
+
+    public static void setDay(int dayOfWeek){
+        today = dayOfWeek;
+    }
+}
