036c620b2df84d88da0194d3dff08363e94f990c
Lavanya Satyan <lsatyan@ucsd.edu>
2019-02-16 22:38:38 -0800

    remove comments

--- app/src/main/java/com/android/personalbest/WalkActivity.java
+++ app/src/main/java/com/android/personalbest/WalkActivity.java
@@ -81,8 +81,6 @@ public class WalkActivity extends AppCompatActivity {
         float intentionalMilesPerHour = Float.parseFloat(MPHTextView.getText().toString());
 
         int elapsedTimeInMilliseconds = walkDataAdapter.getCurrentElapsedTime();
-
-        //Consider changing to float but may cause excessive refactoring in bar chart activity
         int intentionalTimeElapsed = elapsedTimeInMilliseconds/60000;
 
         sharedPrefManager.storeIntentionalWalkStats(Calendar.DAY_OF_WEEK, intentionalStepsTaken, intentionalDistanceInMiles,
