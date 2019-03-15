1aec9bbfb3df4364b6bcef5e065ec9aa0fe2b1a4
Lavanya Satyan <lsatyan@ucsd.edu>
2019-03-14 21:23:59 -0700

    fixed error

--- app/src/main/java/com/android/personalbest/cloud/FirestoreAdapter.java
+++ app/src/main/java/com/android/personalbest/cloud/FirestoreAdapter.java
@@ -14,6 +14,7 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentChange;
 import com.google.firebase.firestore.DocumentSnapshot;
@@ -53,6 +54,7 @@ public class FirestoreAdapter implements CloudstoreService {
     public FirestoreAdapter(Context context){
         this.context = context;
         this.sharedPrefManager = new SharedPrefManager(context);
+        FirebaseApp.initializeApp(context);
         currentAppUser = FirebaseFirestore.getInstance().collection(COLLECTION_KEY);
     }
 
